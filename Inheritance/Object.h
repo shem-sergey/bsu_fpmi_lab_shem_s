61c8489eae933113068999844cdd26b6863adf26
shem-sergey <shem_sergey@tut.by>
2015-02-20 16:40:23 +0300

    Demonstrating abilities of basic class.

--- Inheritance/Object.h
+++ Inheritance/Object.h
@@ -1,6 +1,9 @@
 #ifndef _OBJECT
 #define _OBJECT
-#include <string>
+
+#include <string.h>
+using std::string;
+
 class General_Object
 {
 private: 
@@ -8,10 +11,42 @@ private:
 	double volume;  // CUBIC CENTIMETRES
 	string name;
 public:
-	virtual double get_weight {return weight;}
-	virtual double get_volume {return volume;}
-	string get_name {return name;}
-	virtual double get_density {return weight/volume;}
+	virtual ~General_Object();
+	General_Object() {}
+	General_Object(double, double, string);
+	virtual double get_weight(void);
+	virtual double get_volume(void);
+	string get_name(void);
+	virtual double get_density(void);
+};
+
+General_Object::General_Object(double w, double v, string n)
+{
+	weight = w;
+	volume = v;
+	name = n;	
+}
+
+General_Object::~General_Object() {}
+
+double General_Object::get_weight()
+{
+	return weight;	
+}
+
+double General_Object::get_volume()
+{
+	return volume;	
+}
+
+string General_Object::get_name()
+{
+	return name;	
+}
+
+double General_Object::get_density()
+{
+	return weight/volume;	
 }
 
 #endif
--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include "Object.h"
 
 using namespace std;
 
+void show_gen_obj(General_Object & obj)
+{
+	cout << "Weight of object is " << obj.get_weight() << " grams. Volume is " << obj.get_volume() << " cubic centimertres.";
+	cout << " So density is approximately " << obj.get_density() << " grams per cubic centimeter. "; 
+	cout << "And it is called \"";
+	cout << obj.get_name() << "\"." << endl; 
+}
+
 int main()
 {
-	
+	General_Object book(400, 1500, "Things fall apart"), laptop(2200, 12000, "ProBook 9000"), game(2500, 64000, "Arkham Horror");
+	show_gen_obj(book);
+	show_gen_obj(laptop);
+	show_gen_obj(game);
     system("PAUSE");
     return EXIT_SUCCESS;
 }
