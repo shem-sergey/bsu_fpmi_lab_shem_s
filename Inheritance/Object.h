f1d65dd6d573e5f77fb805f127dd8af472ae2414
shem-sergey <shem_sergey@tut.by>
2015-02-20 17:03:13 +0300

    Inherited class created. General_Object turned into Metric_Object for better understanding. Minor bugs fixed.

--- Inheritance/Object.h
+++ Inheritance/Object.h
@@ -4,47 +4,65 @@
 #include <string.h>
 using std::string;
 
-class General_Object
+class Metric_Object
 {
-private: 
+protected: 
 	double weight; // GRAMS
 	double volume;  // CUBIC CENTIMETRES
 	string name;
 public:
-	virtual ~General_Object();
-	General_Object() {}
-	General_Object(double, double, string);
+	virtual ~Metric_Object();
+	Metric_Object() {}
+	Metric_Object(double, double, string);
 	virtual double get_weight(void);
 	virtual double get_volume(void);
 	string get_name(void);
 	virtual double get_density(void);
 };
 
-General_Object::General_Object(double w, double v, string n)
+class British_Object : public Metric_Object
+{
+public:
+	British_Object() {}
+	~British_Object() {}
+	British_Object(double, double, string);	
+	double get_weight(void) {return this->weight/453.6;} // POUNDS
+	double get_volume(void) {return this->volume/16.4;}  // CUBIC INCHES
+	double get_density(void) {return (this->weight/this->volume)*0.036;}// POUNDS PER CUBIC INCHES
+};
+
+British_Object::British_Object(double w, double v, string n)
 {
 	weight = w;
 	volume = v;
 	name = n;	
 }
 
-General_Object::~General_Object() {}
+Metric_Object::Metric_Object(double w, double v, string n)
+{
+	weight = w;
+	volume = v;
+	name = n;	
+}
 
-double General_Object::get_weight()
+Metric_Object::~Metric_Object() {}
+
+double Metric_Object::get_weight()
 {
 	return weight;	
 }
 
-double General_Object::get_volume()
+double Metric_Object::get_volume()
 {
 	return volume;	
 }
 
-string General_Object::get_name()
+string Metric_Object::get_name()
 {
 	return name;	
 }
 
-double General_Object::get_density()
+double Metric_Object::get_density()
 {
 	return weight/volume;	
 }
--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void show_gen_obj(General_Object & obj)
+void show_met_obj(Metric_Object & obj)
 {
 	cout << "Weight of object is " << obj.get_weight() << " grams. Volume is " << obj.get_volume() << " cubic centimertres.";
 	cout << " So density is approximately " << obj.get_density() << " grams per cubic centimeter. "; 
@@ -11,12 +11,32 @@ void show_gen_obj(General_Object & obj)
 	cout << obj.get_name() << "\"." << endl; 
 }
 
+void show_brit_obj(British_Object & obj)
+{
+	cout << "Weight of object is " << obj.get_weight() << " pounds. Volume is " << obj.get_volume() << " cubic inches.";
+	cout << " So density is approximately " << obj.get_density() << " punds per cubic inch. "; 
+	cout << "And it is called \"";
+	cout << obj.get_name() << "\"." << endl;	
+}
+
 int main()
 {
-	General_Object book(400, 1500, "Things fall apart"), laptop(2200, 12000, "ProBook 9000"), game(2500, 64000, "Arkham Horror");
-	show_gen_obj(book);
-	show_gen_obj(laptop);
-	show_gen_obj(game);
+	{
+		Metric_Object book(400, 1500, "Things fall apart"), laptop(2200, 12000, "ProBook 9000"), board_game(2500, 64000, "Arkham Horror");
+		show_met_obj(book);
+		show_met_obj(laptop);
+		show_met_obj(board_game);
+	}
+	
+	cout << endl;
+	
+	{
+		British_Object book(400, 1500, "Great Expextations"), laptop(2200, 12000, "Dixons Retail"), board_game(2500, 64000, "Lost in London");
+		show_brit_obj(book);
+		show_brit_obj(laptop);
+		show_brit_obj(board_game);
+	}
+	
     system("PAUSE");
     return EXIT_SUCCESS;
 }
